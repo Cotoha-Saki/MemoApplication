9b42a0f5295760e2fb9f7fa299a146c70556c591
Saki <kotoha.saki.14@gmail.com>
2024-02-04 17:52:20 +0900

    commit

--- app/src/main/java/xyz/cotoha/program/memoapplication/MainActivity.kt
+++ app/src/main/java/xyz/cotoha/program/memoapplication/MainActivity.kt
@@ -5,20 +5,32 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Viewの取得
         val btnAdd:Button =findViewById(R.id.btnAdd)
         val lv:ListView =findViewById(R.id.lv)
 
+        //1)アダプターに入れてListViewにセット
         val adapter =ArrayAdapter<String>(
             this,
             android.R.layout.simple_list_item_1,
-            mutableListOf()
+            mutableListOf()//最初は空っぽのリスト/ arrayListOf()
         )
         lv.adapter =adapter
+
+        btnAdd.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("メモの作成")
+                .setMessage("ここに入力")
+                .setPositiveButton("追加",null)
+                .setNegativeButton("キャンセル",null)
+                .show()
+        }
     }
 }
